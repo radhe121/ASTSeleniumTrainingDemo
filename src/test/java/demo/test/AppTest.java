2ffa5516e4210b048b7a33864af7597727e1f8c2
radhe121 <36955317+radhe121@users.noreply.github.com>
2018-03-04 22:31:49 +0530

    Update AppTest.java

--- src/test/java/demo/test/AppTest.java
+++ src/test/java/demo/test/AppTest.java
@@ -12,14 +12,20 @@ public class AppTest {
      
   @Test
   public void verifyHomepageTitle() {
-       
-      System.out.println("launching Chrome browser"); 
+      PropertyConfigurator.configure("log4j.properties");
+	  Logger logger = Logger.getLogger("verifyHomepageTitle");
+	    
+ 
+      logger.info("launching Chrome browser"); 
+      
       System.setProperty("webdriver.chrome.driver","E:\\Desktop\\SelniumJars_Driver\\chromedriver.exe");
       driver = new ChromeDriver();
       driver.get(baseUrl);
+      logger.info("launched flight URL");
       String expectedTitle = "Welcome: Mercury Tours";
       String actualTitle = driver.getTitle();
       Assert.assertEquals(actualTitle, expectedTitle);
+      logger.info("title is capture");
       driver.close();
       //ffff
   }
